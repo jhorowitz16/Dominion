36fb198ca9dde3ccc72ee558eb406076dce6c3f7
joshua.horowitz <joshua.horowitz@zanbato.com>
2016-07-01 13:57:50 -0700

    changed tabs from 6 to 4 spaces

--- game.py
+++ game.py
@@ -7,92 +7,94 @@ DEBUG = True
 
 class Game:
 
-        def __init__(self):
-                # initialize the starting deck here instead
-                self.deck = Deck()
-                self.turn = 0
-                self.deck.fullPrint(self.turn)
+    def __init__(self):
+        # initialize the starting deck here instead
+        self.deck = Deck()
+        self.turn = 0
+        self.deck.fullPrint(self.turn)
 
-        # note the constructor already initialized the deck, plus it is already shuffled 
-        def setup(self):
-                pass
-        
+    # note the constructor already initialized the deck, plus it is already shuffled 
+    def setup(self):
+        pass
+    
 
-        # ABC
-        # Action
-        # Buy
-        # Collect
-        def takeTurn(self):
-                return_info = {} # for playing around with events - to get feedback in main
+    # ABC
+    # Action
+    # Buy
+    # Collect
+    def takeTurn(self):
+        return_info = {} # for playing around with events - to get feedback in main
 
-                self.turn += 1
-                self.deck.drawHand()
-                self.deck.fullPrint(self.turn) # hand right after drawing it
-                return_info['money'] = self.deck.calcHandMoney()
-                return_info['turn'] = self.turn
-                return_info['vp'] = self.deck.total_vp
+        self.turn += 1
+        self.deck.drawHand()
+        self.deck.fullPrint(self.turn) # hand right after drawing it
+        return_info['money'] = self.deck.calcHandMoney()
+        return_info['turn'] = self.turn
+        return_info['vp'] = self.deck.total_vp
 
-                stats = self.simpleCalcPlay()
-                print(stats)
-                self.moneyBuy(stats['money'], True)
-                self.deck.cleanUp()
+        stats = self.simpleCalcPlay()
+        print(stats)
+        self.moneyBuy(stats['money'], True)
+        self.deck.cleanUp()
 
-                return return_info 
-
-
-        # choose what to buy...
-        # if provinces is set to true, buy provinces in addition to silvers and golds
-        def moneyBuy(self, money, provinces=False):
-                # money = self.deck.calcHandMoney()
-                dp("money: " + str(money))
-                if money >= 8 and provinces:
-                        dp("province")
-                        self.deck.addCardType('PROVINCE')
-                elif money < 3:
-                        dp("no money, no buy")
-                elif money < 6:
-                        dp("silver")
-                        self.deck.addCardType('SILVER')
-                else:
-                        dp("gold")
-                        self.deck.addCardType('GOLD')
-
-        # Calculate and play the entire hand
-        # the simple playing code - play everything possible - random order
-        # *** tbd strat - take into account: being able to play everything, more valuable actions (expensive first?)
-        # use a dictionary to keep track of everything for all cards (not just actions)
-        def simpleCalcPlay(self):
-                # start with one action and one buy and no money
-                stats = {'actions': 1, 'buys': 1, 'money': 0, 'vp': 0}
-                i = 0
-                while (i < len(self.deck.hand) and (stats['actions'] > 0)):
-                        print("i is " + str(i))
-                        card = self.deck.hand[i]
-                        dp("this card is worth " + str(card.money))
-                        if card.name in Deck.action_types:
-                                stats['actions'] -= 1
-                                # it's an action card
-                                card.play(self.deck, stats) 
-                        elif card.name in Deck.money_types:
-                                stats['money'] += card.money
-                        else:
-                                stats['vp'] += card.vp
-                        i += 1
-                                # add a play action for each card later - 
-                                # *** - dictionary maps card names to the methods themselves...
-                return stats
+        return return_info 
 
 
-        # play takes a strategy function for how to play action cards (basically everything other than the buying
-        def play(self, strat):
-                pass
 
-        # buy does the buying, also taking a strategy function for the best card(s) to buy
-        def buy(self, strat, *args):
-                strat(args)
+
+    # choose what to buy...
+    # if provinces is set to true, buy provinces in addition to silvers and golds
+    def moneyBuy(self, money, provinces=False):
+        # money = self.deck.calcHandMoney()
+        dp("money: " + str(money))
+        if money >= 8 and provinces:
+            dp("province")
+            self.deck.addCardType('PROVINCE')
+        elif money < 3:
+            dp("no money, no buy")
+        elif money < 6:
+            dp("silver")
+            self.deck.addCardType('SILVER')
+        else:
+            dp("gold")
+            self.deck.addCardType('GOLD')
+
+    # Calculate and play the entire hand
+    # the simple playing code - play everything possible - random order
+    # *** tbd strat - take into account: being able to play everything, more valuable actions (expensive first?)
+    # use a dictionary to keep track of everything for all cards (not just actions)
+    def simpleCalcPlay(self):
+        # start with one action and one buy and no money
+        stats = {'actions': 1, 'buys': 1, 'money': 0, 'vp': 0}
+        i = 0
+        while (i < len(self.deck.hand) and (stats['actions'] > 0)):
+            print("i is " + str(i))
+            card = self.deck.hand[i]
+            dp("this card is worth " + str(card.money))
+            if card.name in Deck.action_types:
+                stats['actions'] -= 1
+                # it's an action card
+                card.play(self.deck, stats) 
+            elif card.name in Deck.money_types:
+                stats['money'] += card.money
+            else:
+                stats['vp'] += card.vp
+            i += 1
+                # add a play action for each card later - 
+                # *** - dictionary maps card names to the methods themselves...
+        return stats
+
+
+    # play takes a strategy function for how to play action cards (basically everything other than the buying
+    def play(self, strat):
+        pass
+
+    # buy does the buying, also taking a strategy function for the best card(s) to buy
+    def buy(self, strat, *args):
+        strat(args)
 
 
 # debug print - same as print iff debug is true
 def dp(print_str):
-        if DEBUG:
-                print(print_str)
+    if DEBUG:
+        print(print_str)
--- main.py
+++ main.py
@@ -10,71 +10,71 @@ from game import Game
 
 
 def turnsUntilEvent():
-        num_turns = []
-        # find out the average number of turns it takes to get some event
-        for i in range(0, 2000):
-                game = Game()
-                game.setup()
-                money = 0
-                money_dist = [] # distribution of the money - to see the progression as the deck comp improves
+      num_turns = []
+      # find out the average number of turns it takes to get some event
+      for i in range(0, 2000):
+            game = Game()
+            game.setup()
+            money = 0
+            money_dist = [] # distribution of the money - to see the progression as the deck comp improves
 
-                # target amount of money here
-                while (money < 8):
-                        turn_results = game.takeTurn()
-                        money = turn_results['money'] 
-                        turn = turn_results['turn'] 
-                        money_dist.append(money)
-                        print('again')
-                print("money distribution: " + str(money_dist))
-                print('done')
-                num_turns.append(turn)
+            # target amount of money here
+            while (money < 8):
+                turn_results = game.takeTurn()
+                money = turn_results['money'] 
+                turn = turn_results['turn'] 
+                money_dist.append(money)
+                print('again')
+            print("money distribution: " + str(money_dist))
+            print('done')
+            num_turns.append(turn)
 
-        print(num_turns)
-        print("average: " + str(sum(num_turns)/len(num_turns)))
+      print(num_turns)
+      print("average: " + str(sum(num_turns)/len(num_turns)))
 
 
-        # with 2000 trials, to get a hand of 5 golds - it takes 46 turns 
-        # 11 turns for first province
+      # with 2000 trials, to get a hand of 5 golds - it takes 46 turns 
+      # 11 turns for first province
 
 # turnsUntilEvent()
 
 
 
 def turnsUntilPoints(num_points):
-        num_turns = []
-        # find out the average number of turns it takes to get some event
-        for i in range(0, 2000):
-                game = Game()
-                game.setup()
-                vp = 0
-                vp_dist = [] # distribution of the victory points- to see the progression as the deck comp improves
+      num_turns = []
+      # find out the average number of turns it takes to get some event
+      for i in range(0, 2000):
+            game = Game()
+            game.setup()
+            vp = 0
+            vp_dist = [] # distribution of the victory points- to see the progression as the deck comp improves
 
-                # target amount of VPs here
-                while (vp < num_points):
-                        turn_results = game.takeTurn()
-                        turn = turn_results['turn'] 
-                        vp = turn_results['vp'] 
-                        vp_dist.append(vp)
-                        print('again')
-                print("victory point distribution: " + str(vp_dist))
-                print('done')
-                num_turns.append(turn)
+            # target amount of VPs here
+            while (vp < num_points):
+                turn_results = game.takeTurn()
+                turn = turn_results['turn'] 
+                vp = turn_results['vp'] 
+                vp_dist.append(vp)
+                print('again')
+            print("victory point distribution: " + str(vp_dist))
+            print('done')
+            num_turns.append(turn)
 
-        print(num_turns)
-        print("average: " + str(sum(num_turns)/len(num_turns)))
+      print(num_turns)
+      print("average: " + str(sum(num_turns)/len(num_turns)))
 
 # turnsUntilPoints(21)
 
 def playGame():
-        game = Game()
-        game.setup()
-        game_over = False
-        while (not game_over):
-                input_string = raw_input("press c to continue, q to quit: ")
-                print('test')
-                
-                if input_string[0] == 'q':
-                        game_over = True
-                turn_results = game.takeTurn()
+      game = Game()
+      game.setup()
+      game_over = False
+      while (not game_over):
+            input_string = raw_input("press c to continue, q to quit: ")
+            print('test')
+            
+            if input_string[0] == 'q':
+                game_over = True
+            turn_results = game.takeTurn()
 
 playGame()
