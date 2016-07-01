468dea682d8303f8a8a26803f10004b3ff8998bf
joshua.horowitz <joshua.horowitz@zanbato.com>
2016-07-01 14:28:31 -0700

    finished tabbing

--- card.py
+++ card.py
@@ -5,93 +5,92 @@
 
 class Card:
 
-      # Card IDs - one unique id for type of card
-      ids = { 'COPPER': 0, 'SILVER' : 1, 'GOLD' : 2,
-            'ESTATE' : 3, 'DUCHY' : 4, 'PROVINCE' : 5,
-            'BLANK' : -1
-      }
-
-      # card costs all in a dictionary
-      costs = { 'COPPER': 0, 'SILVER' : 3, 'GOLD' : 6,
-            'ESTATE' : 2, 'DUCHY' : 5, 'PROVINCE' : 8,
-            'BLANK' : -1
-      }
+    # Card IDs - one unique id for type of card
+    ids = { 'COPPER': 0, 'SILVER' : 1, 'GOLD' : 2,
+    'ESTATE' : 3, 'DUCHY' : 4, 'PROVINCE' : 5,
+    'BLANK' : -1
+    }
+    # card costs all in a dictionary
+    costs = { 'COPPER': 0, 'SILVER' : 3, 'GOLD' : 6,
+    'ESTATE' : 2, 'DUCHY' : 5, 'PROVINCE' : 8,
+    'BLANK' : -1
+    }
       
-      # the unique IDs are based on when they were added to the deck
+    # the unique IDs are based on when they were added to the deck
 
-      # the default card is a blank card (card_id -1 b/c not in a deck and type_id -1 b/c not a real card)
-	def __init__(self, name="", card_id=-1):
-		self.name = name
-		self.cost = self.cardToCost() 
-		self.card_id = card_id # unique id based on the individual card
-		self.type_id = Card.ids[name] # unique id based on the type of card
-            self.money = self.cardToMoney() # copper 1, silver 2, gold 3 - ... tho market 1, etc.
-            self.worth = self.cardToWorth() # for AI...
-            self.vp = self.cardToVP() # end game scoring
-            print("I am worth : " + str(self.money))
-
-
-	# actions can do stuff...
-	def action():
-		# card draw
-		# extra buys
-		# extra money (included in the money value not this)
-		# special stuff like attack cards later...
-            pass
-
-      # for now, everything has no money value other than the coins, which are one under their value
-      def cardToMoney(self):
-            print(str(self.name) + " is a " + str(self.type_id) + " rather than " + str(Card.ids['COPPER']))
-            if self.type_id == Card.ids['COPPER']:
-                print("card to money")
-                return 1
-            elif self.type_id == Card.ids['SILVER']:
-                return 2 
-            elif self.type_id == Card.ids['GOLD']:
-                return 3 
-            else: 
-                return 0 
-
-      # for now the worth of a card is its money value
-      def cardToWorth(self):
-            return self.cardToMoney()
-
-      # for now the worth of a card is only worth points if estate, duchy, or province 
-      def cardToVP(self):
-            if self.type_id == Card.ids['ESTATE']:
-                return 1
-            elif self.type_id == Card.ids['DUCHY']:
-                return 3 
-            elif self.type_id == Card.ids['PROVINCE']:
-                return 6 
-            else: 
-                return 0 
-
-      # move to a database or something ... at some point
-      def cardToCost(self):
-            return Card.costs[self.name] 
+    # the default card is a blank card (card_id -1 b/c not in a deck and type_id -1 b/c not a real card)
+    def __init__(self, name="", card_id=-1):
+        self.name = name
+        self.cost = self.cardToCost() 
+        self.card_id = card_id # unique id based on the individual card
+        self.type_id = Card.ids[name] # unique id based on the type of card
+        self.money = self.cardToMoney() # copper 1, silver 2, gold 3 - ... tho market 1, etc.
+        self.worth = self.cardToWorth() # for AI...
+        self.vp = self.cardToVP() # end game scoring
+        print("I am worth : " + str(self.money))
 
 
-      ##### action card methods here #####
-      # Note: every action card must take and return an "effects" dictionary
-      # effects dictionary has actions and buys (for now)
+    # actions can do stuff...
+    def action():
+        # card draw
+        # extra buys
+        # extra money (included in the money value not this)
+        # special stuff like attack cards later...
+        pass
 
-      # draw 2 cards, gain 1 action
-      def laboratory(deck, stats):
-            deck.drawCards(2) 
-            stats['actions'] += 1 # gain an action
-            return stats 
+    # for now, everything has no money value other than the coins, which are one under their value
+    def cardToMoney(self):
+        print(str(self.name) + " is a " + str(self.type_id) + " rather than " + str(Card.ids['COPPER']))
+        if self.type_id == Card.ids['COPPER']:
+            print("card to money")
+            return 1
+        elif self.type_id == Card.ids['SILVER']:
+            return 2 
+        elif self.type_id == Card.ids['GOLD']:
+            return 3 
+        else: 
+            return 0 
+
+    # for now the worth of a card is its money value
+    def cardToWorth(self):
+        return self.cardToMoney()
+
+    # for now the worth of a card is only worth points if estate, duchy, or province 
+    def cardToVP(self):
+        if self.type_id == Card.ids['ESTATE']:
+            return 1
+        elif self.type_id == Card.ids['DUCHY']:
+            return 3 
+        elif self.type_id == Card.ids['PROVINCE']:
+            return 6 
+        else: 
+            return 0 
+
+    # move to a database or something ... at some point
+    def cardToCost(self):
+        return Card.costs[self.name] 
 
 
+    ##### action card methods here #####
+    # Note: every action card must take and return an "effects" dictionary
+    # effects dictionary has actions and buys (for now)
+
+    # draw 2 cards, gain 1 action
+    def laboratory(deck, stats):
+        deck.drawCards(2) 
+        stats['actions'] += 1 # gain an action
+        return stats 
 
 
 
-	def __repr__(self):
-            return str(self)
 
-      # assuming 2 digits as never going to get more than 99 cards...
-      # append a 0 to the front of single digit IDs
-      def __str__(self):
+
+    def __repr__(self):
+        return str(self)
+
+    # assuming 2 digits as never going to get more than 99 cards...
+    # append a 0 to the front of single digit IDs
+    def __str__(self):
         if self.card_id > -1 and self.card_id < 10:
             return self.name + "_0" + str(self.card_id)
         return self.name + "_" + str(self.card_id)
--- deck.py
+++ deck.py
@@ -9,172 +9,170 @@ DEBUG = False
 
 class Deck:
 
-      # represent the cards in a deck as a list of Card objects
+    # represent the cards in a deck as a list of Card objects
+    # there is no full deck object - all cards are saved in pile, discard or hand
+    # cards = []
+    # pile is the "deck" in the traditional sense of the cards yet to play...
 
-      # there is no full deck object - all cards are saved in pile, discard or hand
+    money_types = ['COPPER', 'SILVER', 'GOLD']
+    victory_types = ['ESTATE', 'DUCHY', 'PROVINCE']
+    action_types = ['VILLAGE', 'SMITHY', 'LABORATORY']
+    all_types = money_types + victory_types + action_types
 
-	# cards = []
-      # pile is the "deck" in the traditional sense of the cards yet to play...
+    def __init__(self, deck_id=0, player="player", name="deck"):
+        dp("INIT")
+        self.deck_id = deck_id 
+        self.player = player
+        self.name = name
 
-	money_types = ['COPPER', 'SILVER', 'GOLD']
-	victory_types = ['ESTATE', 'DUCHY', 'PROVINCE']
-      action_types = ['VILLAGE', 'SMITHY', 'LABORATORY']
-	all_types = money_types + victory_types + action_types
-
-	def __init__(self, deck_id=0, player="player", name="deck"):
-            dp("INIT")
-		self.deck_id = deck_id 
-		self.player = player
-		self.name = name
-
-            self.hand = []
-            self.discard = []
-            self.pile = [] 
-            dp("USING SETUP DECK")
-            self.setupDeck() # 7 coppers, 3 estates
+        self.hand = []
+        self.discard = []
+        self.pile = [] 
+        dp("USING SETUP DECK")
+        self.setupDeck() # 7 coppers, 3 estates
                 
-		self.total_vp = self.calcTotalVPs()
-		self.total_worth = self.calcTotalWorth()
-            self.av_worth = self.total_worth / len(self.pile)
-            self.count = 0
+        self.total_vp = self.calcTotalVPs()
+        self.total_worth = self.calcTotalWorth()
+        self.av_worth = self.total_worth / len(self.pile)
+        self.count = 0
 
       ###### add and remove cards ######
 
-	# two versions of add card - one that takes a type (string) and one that takes an actual card object
-	def addCard(self, card):
-		if card:
-                self.discard += [card]
-			self.count += 1
-			self.av_worth += (self.av_worth*self.count + card.worth) / self.count
-			self.total_vp += card.vp
-		else:
-                dp("card not found")
-			return False
+    # two versions of add card - one that takes a type (string) and one that takes an actual card object
+    def addCard(self, card):
+        if card:
+            self.discard += [card]
+            self.count += 1
+            self.av_worth += (self.av_worth*self.count + card.worth) / self.count
+            self.total_vp += card.vp
+        else:
+            dp("card not found")
+            return False
 
-	def addCardType(self, type_str):
-		if type_str in Deck.all_types:
-			# make a card and add it
-			new_card = Card(type_str, self.count)
-			self.addCard(new_card)
-		else:
-                dp("card type not found")
-			return False
+    def addCardType(self, type_str):
+        if type_str in Deck.all_types:
+            # make a card and add it
+            new_card = Card(type_str, self.count)
+            self.addCard(new_card)
+        else:
+            dp("card type not found")
+            return False
 
-	# remove a card based on its unique id
-	def removeCard(self, id):
+    # remove a card based on its unique id
+    def removeCard(self, id):
             pass
 
 
-      ###### helper methods for simple calculations ###### 
+    ###### helper methods for simple calculations ###### 
 
-      # calculate the total 'worth' of the deck based on the initial list of cards passed into the constructor 
-      def calcTotalWorth(self):
-            total = 0
-            for card in self.pile:
-                total += card.worth
-            return total
+    # calculate the total 'worth' of the deck based on the initial list of cards passed into the constructor 
+    def calcTotalWorth(self):
+        total = 0
+        for card in self.pile:
+            total += card.worth
+        return total
 
-      # count all the victory points in the initial list of cards
-      def calcTotalVPs(self):
-            total = 0
-            everything = self.pile + self.discard + self.hand
-            for card in everything:
-                total += card.worth
-            
-            return total
-     
-      # not sure if this will be useful...
-      def calcTotalMoney(self):
-            total = 0
-            for card in self.pile:
-                total += card.money
-            return total
-      
-      # how much plyaer can buy (include action cards that directly provide money cards)
-      # this should be called after all action cards are played - b/c player can gain extra cards
-      def calcHandMoney(self):
-            total = 0
-            for card in self.hand:
-                total += card.money
-            return total
+    # count all the victory points in the initial list of cards
+    def calcTotalVPs(self):
+        total = 0
+        everything = self.pile + self.discard + self.hand
+        for card in everything:
+            total += card.worth
+        
+        return total
 
+    # not sure if this will be useful...
+    def calcTotalMoney(self):
+        total = 0
+        for card in self.pile:
+            total += card.money
+        return total
 
-      ###### shuffling ######
-
-      # actually shuffle the pile... no more deck
-      def reshuffle(self):
-            dp("reshuffle")
-            if len(self.discard) > 1:
-                dp("actually shuffling")
-                shuffle(self.discard)
-            self.pile += self.discard
-            self.discard = []
+    # how much plyaer can buy (include action cards that directly provide money cards)
+    # this should be called after all action cards are played - b/c player can gain extra cards
+    def calcHandMoney(self):
+        total = 0
+        for card in self.hand:
+            total += card.money
+        return total
 
 
+    ###### shuffling ######
 
-      ###### game actions ######
-
-      # prep the initial deck of 7 coppers and 3 estates
-      # shuffle the pile
-      def setupDeck(self):
-            for i in range(7):
-                copper = Card("COPPER", i)
-                self.pile.append(copper)
-            for j in range(7, 10):
-                estate = Card("ESTATE", j)
-                self.pile.append(estate)
-            self.count = 10
-            shuffle(self.pile)
-
-      # discard the hand including played cards
-      def cleanUp(self):
-            self.discard += self.hand
-            self.hand = []
-
-
-      # draw an integer number of cards (5 for hand)
-      # give number of cards as a parameter
-      def drawCards(self, numCards):
-            if not self.pile:
-                # no pile... problem... need to shuffle
-                self.reshuffle()
-            left_to_deal = numCards 
-            while left_to_deal > 0:
-                dp(self.pile)
-                if len(self.pile) > 0:
-                      self.hand.append(self.pile.pop())
-                      left_to_deal -= 1 
-                else:
-                      # need to reshuffle
-                      self.reshuffle()
-
-      # draw hand default is 5 cards, without expansions....
-      def drawHand(self):
-            self.drawCards(5)
-
-      # end of turn, discard hand (and all played cards)
-      def handToDiscard(self):
-            discard += hand
-            hand = []
+    # actually shuffle the pile... no more deck
+    def reshuffle(self):
+        dp("reshuffle")
+        if len(self.discard) > 1:
+            dp("actually shuffling")
+            shuffle(self.discard)
+        self.pile += self.discard
+        self.discard = []
 
 
 
-      ###### string related utils ######
+    ###### game actions ######
 
-      def __repr__(self):
-            return self.name + '_' + str(self.deck_id)
+    # prep the initial deck of 7 coppers and 3 estates
+    # shuffle the pile
+    def setupDeck(self):
+        for i in range(7):
+            copper = Card("COPPER", i)
+            self.pile.append(copper)
+        for j in range(7, 10):
+            estate = Card("ESTATE", j)
+            self.pile.append(estate)
+        self.count = 10
+        shuffle(self.pile)
 
-      def __str__(self):
-            return str(self.pile)
+    # discard the hand including played cards
+    def cleanUp(self):
+        self.discard += self.hand
+        self.hand = []
 
-      def fullPrint(self, turn=-1):
-            if turn > -1:
-                print("\n\n=================== turn " + str(turn) + " ====================")
+
+    # draw an integer number of cards (5 for hand)
+    # give number of cards as a parameter
+    def drawCards(self, numCards):
+        if not self.pile:
+            # no pile... problem... need to shuffle
+            self.reshuffle()
+        left_to_deal = numCards 
+        while left_to_deal > 0:
+            dp(self.pile)
+            if len(self.pile) > 0:
+                  self.hand.append(self.pile.pop())
+                  left_to_deal -= 1 
             else:
-                print("\n\n===============================================")
-            print("Hand: " + str(self.hand))
-            print("Pile: " + str(self.pile))
-            print("Discard: " + str(self.discard))
+                  # need to reshuffle
+                  self.reshuffle()
+
+    # draw hand default is 5 cards, without expansions....
+    def drawHand(self):
+        self.drawCards(5)
+
+    # end of turn, discard hand (and all played cards)
+    def handToDiscard(self):
+        discard += hand
+        hand = []
+
+
+
+    ###### string related utils ######
+
+    def __repr__(self):
+        return self.name + '_' + str(self.deck_id)
+
+    def __str__(self):
+        return str(self.pile)
+
+    def fullPrint(self, turn=-1):
+        if turn > -1:
+            print("\n\n=================== turn " + str(turn) + " ====================")
+        else:
+            print("\n\n===============================================")
+        print("Hand: " + str(self.hand))
+        print("Pile: " + str(self.pile))
+        print("Discard: " + str(self.discard))
 
 # debug print - same as print iff debug is true
 def dp(print_str):
